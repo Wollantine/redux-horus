494a91a0a0c3f5f7d83c82eb11d0acae5e328f73
Marc Rodríguez <nebrios2@gmail.com>
2018-11-07 20:19:38 +0100

    Adapt code to API

--- src/redux-horus.ts
+++ src/redux-horus.ts
@@ -2,12 +2,12 @@ import {curry} from '@typed/curry'
 
 export type Dictionary<T> = {[key: string]: T}
 
-export type TAction = {
+export type Action = {
     [key: string]: any;
     type: string;
 }
 
-export type TReducer<T> = (state: T | undefined, action: TAction) => T
+export type Reducer<T> = (state: T | undefined, action: Action) => T
 
 const propOr = (def: any, prop: string, obj: any): any => (
     (obj && obj[prop]) || def
@@ -20,51 +20,51 @@ const pathEq = (path: string[], value: any, obj: any): boolean => (
     )
 
 export const hash = curry(<T>(
-    reducer: (state: T, action: TAction) => Dictionary<(state?: T, action?: TAction) => T>,
+    reducer: (state: T, action: Action) => Dictionary<(state?: T, action?: Action) => T>,
     initialState: T | (() => T)
-): TReducer<T> => (
-    (state: T | undefined, action: TAction): T => {
+): Reducer<T> => (
+    (state: T | undefined, action: Action): T => {
         const initializedState = (state === undefined)
             ? typeof initialState === 'function' ? (initialState as () => T)() : initialState
             : state
         const defaultCase = () => initializedState
         const cases = reducer(initializedState, action)
-        const matchedCase: (state?: T, action?: TAction) => T = propOr(defaultCase, action.type, cases)
+        const matchedCase: (state?: T, action?: Action) => T = propOr(defaultCase, action.type, cases)
         return matchedCase(state, action)
     }
 ))
     
 export const when = curry(<T>(
-    condition: ({state, action}: {state: T, action: TAction}) => boolean,
-    reducer: TReducer<T>,
+    condition: (state: T, action: Action) => boolean,
+    reducer: Reducer<T>,
     initialState: T,
-): TReducer<T> => (state: T = initialState, action: TAction): T => (
-    condition({state, action})
+): Reducer<T> => (state: T = initialState, action: Action): T => (
+    condition(state, action)
         ? reducer(state, action)
         : state
 ))
 
 export const whenAction = curry(<T>(
-    condition: (action: TAction) => boolean,
-    reducer: TReducer<T>,
+    condition: (action: Action) => boolean,
+    reducer: Reducer<T>,
     initialState: T,
-): TReducer<T> =>
-    when(({action}: {action: TAction}) => condition(action), reducer, initialState)
+): Reducer<T> =>
+    when((action: Action) => condition(action), reducer, initialState)
 )
 
 export const whenState = curry(<T>(
     condition: (state: T) => boolean,
-    reducer: TReducer<T>,
+    reducer: Reducer<T>,
     initialState: T,
-): TReducer<T> =>
+): Reducer<T> =>
     when(({state}: {state: T}) => condition(state), reducer, initialState)
 )
 
 export const branch = curry(<T>(
-    condition: (state: T | undefined, action: TAction) => boolean,
-    thenReducer: TReducer<T>,
-    elseReducer: TReducer<T>
-): TReducer<T> => (
+    condition: (state: T | undefined, action: Action) => boolean,
+    thenReducer: Reducer<T>,
+    elseReducer: Reducer<T>
+): Reducer<T> => (
     (state, action) => (
         condition(state, action)
             ? thenReducer(state, action)
@@ -76,6 +76,6 @@ export const has = curry((path: string, value: any, obj: any) => (
     pathEq(path.split('.'), value, obj)
 ))
 
-export const is = curry((type: string, action: TAction) => (
+export const is = curry((type: string, action: Action) => (
     has('type', type, action)
 ))
